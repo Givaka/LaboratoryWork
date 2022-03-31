065e573626a4b305af4c1b93cb92bc497a7a0732
givaka <belolnik21@gmail.com>
2022-03-31 22:43:34 +0500

    add lab from 2 to 6

--- src/SecondSemester/lab_1/MyPicture.java
+++ src/SecondSemester/lab_1/MyPicture.java
@@ -17,27 +17,69 @@ public class MyPicture extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE); // метод завершения программы после закрытия окна
     }
 
+    public static void main(String[] args) {
+        new MyPicture("Буб");
+    }
+
+    public void paint (Graphics g){
+        g.setColor(Color.cyan);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        enterNum(g);
+    }
+
     public void pictureStatic (Graphics g){
+        g.setColor(Color.red);
+        for (int i = 0; !((getWidth()/50 == i) || (getHeight()/50 == i)); i++){
+            g.fillRect(50*i,50*i,50,50);    //  Вывод квадратика со смещением
+        }
 
-    }
-
-    public void pictureDynamic (Graphics g){
-
-    }
-
-    public void enterNum(){
-        Scanner in = new Scanner(System.in);
-        int x = in.nextInt();
-        switch (x) {
-            case 1:
-
-                break;
-            case 2:
-                break;
+        g.setColor(Color.blue);
+        for (int i = 0; !((getWidth()/50 == i) || (getHeight()/50 == i)); i++){
+            g.fillRect(50*(getWidth()/50 - i - 1),50*i,50,50);    //  Вывод квадратика со смещением
         }
     }
 
-    public static void main(String[] args) {
+    public void pictureDynamic (Graphics g) {
+        g.setColor(Color.red);
+        for (int i = 0; !((getWidth() / 50 == i) || (getHeight() / 50 == i)); i++) {
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            } finally {
+                g.fillRect(50 * i, 50 * i, 50, 50);    //  Вывод квадратика со смещением
+            }
+        }
 
+        g.setColor(Color.blue);
+        for (int i = 0; !((getWidth()/50 == i) || (getHeight()/50 == i)); i++) {
+            try {
+                Thread.sleep(150);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+                break;
+            } finally {
+                g.fillRect(50*(getWidth()/50 - i - 1),50*i,50,50);    //  Вывод квадратика со смещением
+            }
+        }
     }
+
+    public void enterNum(Graphics g){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Выберите режим вывод (1 - статический; 2 - динамический.): ");
+        int x = in.nextInt();
+        switch (x) {
+            case 1:
+                pictureStatic(g);
+                break;
+            case 2:
+                pictureDynamic(g);
+                break;
+            default:
+                    System.exit(0);
+        }
+    }
+
+
 }
--- src/Test.java
+++ src/Test.java
@@ -1,24 +1,9 @@
+
 public class Test {
     public static void main(String[] args) {
-        for (int i = 1; i < 14 ; i++){
-            if ( i > 9 )
-                for (int k = 0; k <= 10; k++)
-                    System.out.print(" ");
-            else {
-                for (int k = 0; k <= 10 - i; k++)
-                    System.out.print(" ");
+        int[] massStringA = new int[]{10, 5 ,2};
 
-                for (int j = 1; j <= i; j++)
-                    System.out.print(j);
-            }
-
-            if ( i > 4 ){
-                System.out.print(" ");
-                for (int v = 14 - i; v > 0 ; v--)
-                    System.out.print(v);
-            }
-
-            System.out.println();
-        }
+        for (int a: massStringA)
+            System.out.println(a);
     }
 }
